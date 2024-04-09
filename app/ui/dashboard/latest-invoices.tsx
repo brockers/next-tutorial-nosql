f535182371bdb660b29ca42def38c8f1cee0e725
Bobby Rockers <rrockers@gmail.com>
2024-04-09 10:40:24 -0500

    Got the invoices working.

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -2,12 +2,15 @@ import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
-import { LatestInvoice } from '@/app/lib/definitions';
-export default async function LatestInvoices({
-  latestInvoices,
-}: {
-  latestInvoices: LatestInvoice[];
-}) {
+// import { LatestInvoice } from '@/app/lib/definitions';
+// export default async function LatestInvoices({
+//   latestInvoices,
+// }: {
+//   latestInvoices: LatestInvoice[];
+// }) {
+export default async function LatestInvoices(invObj){
+  // Ripped out all the typing to make it work... no have to handle the untyped stuff
+  const latestInvoices = invObj.latestInvoices;
   return (
     <div className="flex w-full flex-col md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
